7e988a48627b036933eea42ccfe2269e237112c8
unknown <faridmumtaz9373@gmail.com>
2022-09-04 19:31:06 +0530

    step 2 completed

--- test_calc.py
+++ test_calc.py
@@ -7,5 +7,8 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(string_calc.add("1"),1)
         self.assertEqual(string_calc.add("1,2"),3)
 
+    def test_unknown_amount_of_numbers(self):
+        self.assertEqual(string_calc.add("8,17,244,690,812,990"),2761)
+
 if __name__ == '__main__':
     unittest.main()
